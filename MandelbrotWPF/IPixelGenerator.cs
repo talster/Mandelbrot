bb0235b9e7090ac086309b5897c0000f3a9e91c3
talster <talster@gmail.com>
2015-01-25 01:52:31 +0200

    Added interfaces

---
+++ MandelbrotWPF/IPixelGenerator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MandelbrotWPF
+{
+    public interface IPixelGenerator
+    {
+        /// <summary>
+        /// Returns the HTML Color for a given pixel 
+        /// </summary>
+        /// <param name="x">X coordinates</param>
+        /// <param name="y">Y coordinates</param>
+        /// <returns>The HTML Color of the given pixel
+        /// See http://html-color-codes.info/ for examples (e.g., "#FF0000" is red)
+        /// </returns>
+        string GetPixelColor(double x, double y);
+    }
+}
---
+++ MandelbrotWPF/SimplePictureGenerator.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace MandelbrotWPF
+{
+    public class SimplePictureGenerator : IPictureGenerator
+    {
+        /// <summary>
+        /// Initialize the Picture Generator
+        /// </summary>
+        /// <param name="pixelGenerators">a list of Pixel generators</param>
+        /// <returns>True if initialization succeeded. False otherwise</returns>
+        public bool Init(List<IPixelGenerator> pixelGenerators)
+        {
+            if (pixelGenerators.Count == 0)
+                return false;
+            return true;
+        }
+
+        /// <summary>
+        /// Generate an image spanning from [xLeft ... xRight] and [yBottom..yTop] 
+        /// with the specified number of rows and columns
+        /// </summary>
+        /// <param name="xLeft">The smallest X coordinate of the generated image</param>
+        /// <param name="xRight">The largest X coordinate of the generated image</param>
+        /// <param name="columns">The number of columns in the image (spanning from xLeft to xRight)</param>
+        /// <param name="yBottom">The smallest Y coordinate of the generated image</param>
+        /// <param name="yTop">The largest Y coordinate of the generated image</param>
+        /// <param name="rows">The number of rows in the image (spanning from yBottom to xTop)</param>
+        /// <returns>An image with rows x columns pixels. 
+        /// The X coordinates of all pixels span from xLeft to xRight.
+        /// The Y coordinates of all pixels span from yBottom to yTop</returns>
+        public BitmapImage GenerateImage(   double xLeft,   double xRight,  int columns,
+                                            double yBottom, double yTop,    int rows)
+        {
+            return new BitmapImage();
+        }
+    }
+}
